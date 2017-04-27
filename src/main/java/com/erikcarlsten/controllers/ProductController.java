dd04579894d7ef6f339d8b05e9b9872f3c49cbc1
Erik S Carlsten <ecarlste@gmail.com>
2017-04-27 10:51:30 -0500

    Fixed request mapping path on the product controller

--- src/main/java/com/erikcarlsten/controllers/ProductController.java
+++ src/main/java/com/erikcarlsten/controllers/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
 
-    @RequestMapping("/")
+    @RequestMapping("/product")
     public String getProduct() {
         return "product";
     }
